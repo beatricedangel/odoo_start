8d21c4ef050f6cc3e250e70873e46b320af98693
beatricedangel <147610678+beatricedangel@users.noreply.github.com>
2024-03-10 16:08:20 +0100

    Create __manifest__.py

---
+++ addons/prenotazioni/__manifest__.py
@@ -0,0 +1,13 @@
+{
+'name': 'Prenotazioni',
+'summary': 'Management system prenotazioni',
+'author': 'Toeletta spa',
+'category': 'Prenotazioni',
+'version': '1.0',
+'sequence' : 1,
+'depends': ['base'],
+'data': [
+        'security/ir.model.access.csv',
+        'views/prenotazioni_view.xml' 
+        ]
+}
